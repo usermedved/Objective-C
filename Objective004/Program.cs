4113c0b2b77246a75e790e2ecfc881d0fdeb2f10
Sergei Medvedev <clip1988@yandex.ru>
2022-08-11 14:21:04 +0300

    Objective 004 project

---
+++ Objective004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberB > max) max = numberB; 
+if(numberС > max) max = numberС;
+Console.Write("max = ");
+Console.WriteLine(max);
