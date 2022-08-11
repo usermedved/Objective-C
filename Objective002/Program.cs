d5c0b37f7ee32807149b5821e9ffcded8dda2ad7
Sergei Medvedev <clip1988@yandex.ru>
2022-08-11 14:02:15 +0300

    Objective002 project

---
+++ Objective002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max;
+if(numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+Console.WriteLine(max);
