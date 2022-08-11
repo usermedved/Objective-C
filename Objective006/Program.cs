edfa7d9c73787390fc0bf9be4570b672d3ed368c
Sergei Medvedev <clip1988@yandex.ru>
2022-08-11 16:15:51 +0300

    Objectiv 006 project

---
+++ Objective006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//Console.Write(numberA / number);
+if(numberA % 2==0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число не четное");
+}
